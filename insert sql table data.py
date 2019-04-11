2cc47a5bab8bb25c5f10c9e2980f918992df017b
leo leung <180133517@stu.vtc.edu.hk>
2019-04-11 23:20:23 +0800

    added foregin keys and table data throgh .py no.2 try

--- insert sql table data.py
+++ insert sql table data.py
@@ -3,8 +3,22 @@ from app.models import User, Post,Product,ProductMysqlServer,ProductXDevAPI,Prod
 from datetime import datetime, timedelta
 import unittest
 
-MysqlServer=Product(title="MysqlServer")
-XDevAPI=Product(title="XDevAPI")
-MySQLNDBCluster=Product(title="MySQLNDBCluster")
+db.session.remove()
+db.drop_all()
+db.create_all()
+
+
+p1=Product(title="MysqlServer")
+p2=Product(title="XDevAPI")
+p3=Product(title="MySQLNDBCluster")
 
 MySQL8_0ReferenceManual=ProductMysqlServer(url="https://dev.mysql.com/doc/refman/8.0/en/",name="MySQL8_0ReferenceManual",title_id="MysqlServer")
+MySQL5_7ReferenceManual=ProductMysqlServer(url="https://dev.mysql.com/doc/refman/5.7/en/",name="MySQL 5.7 Reference Manual",title_id="MysqlServer")
+
+db.session.add(p1)
+db.session.add(p2)
+db.session.add(p3)
+db.session.add(MySQL8_0ReferenceManual)
+db.session.add(MySQL5_7ReferenceManual)
+
+db.session.commit()
